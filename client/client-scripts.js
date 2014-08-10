c51315388631bb29c2192d26ae5863b905def9db
Mark D Rushton <rushtonmd@gmail.com>
2014-08-09 19:53:08 -0700

    Initial commit. Modified cfs tutorial into Brinx Photo Gallery

---
+++ client/client-scripts.js
@@ -0,0 +1,135 @@
+UI.body.events({
+    'change #fileInput': function(event) {
+        FS.Utility.eachFile(event, function(file) {
+            var insertedPhoto = Images.insert(file, function(err, fileObj) {
+                //Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+                //var readStream = fileObj.createReadStream('master');
+            });
+            MediaItems.insert({
+                title: insertedPhoto.name(),
+                description: '(no description)',
+                rank: new Date().getTime(),
+                file: insertedPhoto
+            });
+
+        });
+
+    },
+
+    'click .save-changes-to-media': function(event) {
+
+        var mediaID = $("button.save-changes-to-media").attr('media-id');
+        var newDescription = $(".modal-body .media-description").val();
+        var newTitle = $(".modal-header .media-title").val();
+
+
+        console.log(newTitle);
+        MediaItems.update({
+            "_id": mediaID
+        }, {
+            $set: {
+                "description": newDescription,
+                "title": newTitle
+            }
+        });
+
+        $('#myModal').modal('hide');
+    },
+
+    'click .edit-image': function(event) {
+        var mediaID = $(event.currentTarget).attr('media-id');
+        $("button.save-changes-to-media").attr('media-id', mediaID);
+        var newTitle = $("div.media-item[media-id='" + mediaID + "']").find(".media-heading").html();
+        $(".modal-header .media-title").val(newTitle);
+        var newBody = $("div.media-item[media-id='" + mediaID + "']").find(".media-body p").html();
+        $(".modal-body .media-description").val(newBody);
+        var imageSource = $("div.media-item[media-id='" + mediaID + "']").find("img.media-object").attr('full_size_src');
+        $(".modal-body img.media-object").attr('src', imageSource);
+        var metaWidth = $("div.media-item[media-id='" + mediaID + "']").find("img.media-object").attr('meta_width');
+        var metaHeight = $("div.media-item[media-id='" + mediaID + "']").find("img.media-object").attr('meta_height');
+        $(".modal-body span.height-width").html(metaWidth + " x " + metaHeight);
+        console.log("w" + metaWidth);
+    },
+
+    'click .delete-image': function(event) {
+        var mediaID = $(event.currentTarget).attr('media-id');
+        $("div.media-item[media-id='" + mediaID + "']").fadeOut(function() {
+            MediaItems.update({
+                "_id": mediaID
+            }, {
+                $set: {
+                    "deleted": true
+                }
+            });
+        });
+
+    }
+
+});
+
+Template.images.images = function() {
+    return Images.find({}, {
+        sort: {
+            rank: -1
+        }
+    });
+};
+
+Template.mediaItems.mediaItems = function() {
+    return MediaItems.find({}, {
+        sort: {
+            rank: -1
+        }
+    });
+};
+
+Template.mediaItem.image = function() {
+    //return this.file.getFileRecord();
+    return Images.findOne(this.file._id);
+};
+
+
+SimpleRationalRanks = {
+    beforeFirst: function(firstRank) {
+        return firstRank + 1;
+    },
+    between: function(beforeRank, afterRank) {
+        return (beforeRank + afterRank) / 2;
+    },
+    afterLast: function(lastRank) {
+        return lastRank - 1;
+    }
+};
+
+Template.mediaItems.rendered = function() {
+
+    this.$('#media-list').sortable({
+        stop: function(event, ui) { // fired when an item is dropped
+            var el = ui.item.get(0),
+                before = ui.item.prev().get(0),
+                after = ui.item.next().get(0);
+
+            el2 = el;
+
+            var newRank;
+            if (!before) { // moving to the top of the list
+                newRank = SimpleRationalRanks.beforeFirst(UI.getElementData(after).rank);
+
+            } else if (!after) { // moving to the bottom of the list
+                newRank = SimpleRationalRanks.afterLast(UI.getElementData(before).rank);
+
+            } else {
+                newRank = SimpleRationalRanks.between(
+                    UI.getElementData(before).rank,
+                    UI.getElementData(after).rank);
+            }
+
+            console.log(newRank);
+            MediaItems.update(UI.getElementData(el)._id, {
+                $set: {
+                    rank: newRank
+                }
+            });
+        }
+    });
+};
--- collections/images.js
+++ collections/images.js
@@ -1,22 +1,41 @@
 //Set Cache Control headers so we don't overload our meteor server with http requests
 FS.HTTP.setHeadersForGet([
     ['Cache-Control', 'public, max-age=31536000']
-    ]);
+]);
 
 //Create the master store
-var masterStore = new FS.Store.GridFS("master");
+var masterStore = new FS.Store.FileSystem("master");
 
 //Create a thumbnail store
-var thumbnailStore = new FS.Store.GridFS("thumbnail", {
+var thumbnailStore = new FS.Store.FileSystem("thumbnail", {
     //Create the thumbnail as we save to the store.
     transformWrite: function(fileObj, readStream, writeStream) {
         /* Use graphicsmagick to create a 300x300 square thumbnail at 100% quality,
-        * orient according to EXIF data if necessary and then save by piping to the 
-        * provided writeStream */
-        gm(readStream, fileObj.name).resize(300,300,"^")
-        .gravity('Center').crop(300, 300).quality(100).autoOrient().stream().pipe(writeStream);
+         * orient according to EXIF data if necessary and then save by piping to the
+         * provided writeStream */
+
+        gm(readStream, fileObj.name)
+            .size({
+                bufferStream: true
+            }, Meteor.bindEnvironment(
+                function(err, size) {
+                    if (!err) {
+                        fileObj.update({
+                            $set: {
+                                'metadata.width': size.width,
+                                'metadata.height': size.height
+                            }
+                        });
+                    }
+                },
+                function(error) {
+                    console.log('Error in bindEnvironment:', error);
+                }))
+            .resize(300, 300, "^").gravity('Center').crop(300, 300)
+            .quality(75).autoOrient().stream().pipe(writeStream);
     }
 });
+
 
 //Create globally scoped Images collection.
 Images = new FS.Collection("images", {
@@ -27,13 +46,27 @@ Images = new FS.Collection("images", {
             contentTypes: ['image/*'],
             extensions: ['png', 'jpg', 'jpeg', 'gif']
         },
-        onInvalid: function (message) {
-            if(Meteor.isClient){
+        onInvalid: function(message) {
+            if (Meteor.isClient) {
                 alert(message);
-            }else{
+            } else {
                 console.warn(message);
             }
         }
+    }
+});
+
+MediaItems = new Meteor.Collection('mediaItems');
+
+MediaItems.allow({
+    insert: function(userId, file) {
+        return true;
+    },
+    update: function(userId, file, fields, modifier) {
+        return true;
+    },
+    remove: function(userId, file) {
+        return true;
     }
 });
 
@@ -55,10 +88,28 @@ Images.allow({
 });
 
 
-//If we're on the server publish the collection, otherwise we are on the client and we should subscribe to the publication.
-if(Meteor.isServer){
 
-    Meteor.publish('images', function () {
+
+//If we're on the server publish the collection, otherwise we are on the client and we should subscribe to the publication.
+if (Meteor.isServer) {
+
+    // TODO: Create a query to return the right stuff for the api call
+    HTTP.publish({
+        name: 'api/images-list'
+    }, function(data) {
+        
+        // this.userId, this.query, this.params
+        return MediaItems.find({});
+    });
+
+    Meteor.publish('mediaItems', function() {
+        return MediaItems.find({
+            'deleted': {
+                '$ne': true
+            }
+        });
+    });
+    Meteor.publish('images', function() {
         /*Uncomment this and comment out returning the cursor to see publication issue*/
 
         // var self = this;
@@ -85,6 +136,7 @@ if(Meteor.isServer){
 
     });
 
-}else{
+} else {
     Meteor.subscribe('images');
+    Meteor.subscribe('mediaItems');
 }
